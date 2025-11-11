b0ff5a2df27bee80986df208a0dedfcb6d0d3520
Charly Peralta <dan.bsas22@gmail.com>
2025-11-11 19:52:08 -0300

    11-11-2025

--- src/Components/Workspace-Components/Workspace-new-components/CreateWorkspaceBackground.jsx
+++ src/Components/Workspace-Components/Workspace-new-components/CreateWorkspaceBackground.jsx
@@ -1,4 +1,4 @@
-import '../Workspace-components/WorkspaceComponent.css'
+import '../Workspace-components//Workspace-components/workspaceComponent.css'
 import { RiHome6Line } from "react-icons/ri";
 import CreateWorkspaceComponent from './CreateWorkspaceComponent';
 
